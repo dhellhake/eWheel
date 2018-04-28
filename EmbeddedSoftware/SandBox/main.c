cabc5185853ab6e77c8b69245abdf17e24bba3e2
dhellhake <dominik_hellhake@msn.com>
2018-04-28 10:15:38 +0200

    SandBox initial code

---
+++ EmbeddedSoftware/SandBox/main.c
@@ -0,0 +1,21 @@
+/*
+ * SandBox.c
+ *
+ * Created: 28.04.2018 10:08:12
+ * Author : domin
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
