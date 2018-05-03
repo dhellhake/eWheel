ee84358d3d7aa0a3ff48296a6186619b8dbf2162
dhellhake <dominik_hellhake@msn.com>
2018-05-03 21:27:50 +0200

    Implemented LowLevel SPI communication

--- EmbeddedSoftware/SandBox/LowLevel/SPI/SPI.cpp
+++ EmbeddedSoftware/SandBox/LowLevel/SPI/SPI.cpp
@@ -47,22 +47,12 @@ void SPIPort::SPIInit()
 	pin_set_peripheral_function(PINMUX_PA10C_SERCOM0_PAD2);
 	pin_set_peripheral_function(PINMUX_PA11C_SERCOM0_PAD3);
 	
-	//Configure SERCOM0
-	SERCOM_SPI_CTRLA_Type ctrla;
-	ctrla.bit.DORD = 0;		//MSB first
-	ctrla.bit.CPHA = 0;		//CPHA mode 0
-	ctrla.bit.CPOL = 0;		//CPOL mode 0
-	ctrla.bit.FORM = 0;		//SPI frame
-	ctrla.bit.DIPO = 0;		//MISO on PAD0
-	ctrla.bit.DOPO = 1;		//MOSI on PAD2; SCK on PAD3; SS on PAD 1
-	ctrla.bit.MODE = 3;		//Master mode	
-	SERCOM0->SPI.CTRLA.reg = ctrla.reg;
+	//Configure SERCOM0	
+	SERCOM0->SPI.CTRLA.reg =	(0x3 << SERCOM_SPI_CTRLA_MODE_Pos) |
+								(1 << SERCOM_SPI_CTRLA_DOPO_Pos);
+								
+	SERCOM0->SPI.CTRLB.reg =	(1 << SERCOM_SPI_CTRLB_RXEN_Pos);
 	
-	SERCOM_SPI_CTRLB_Type ctrlb;
-	ctrlb.bit.RXEN = 1;   // Receive is enabled
-	ctrlb.bit.MSSEN = 0;  // Software controlled Slave Select
-	ctrlb.bit.CHSIZE = 0;  // 8-Bit character transfer	
-	SERCOM0->SPI.CTRLB.reg = ctrlb.reg;
 	
 	SERCOM0->SPI.BAUD.reg = (get_gclk_hz(SERCOM0_GCLK_ID_CORE) / (2 * 10000000)) - 1;
 	
@@ -78,4 +68,9 @@ uint8_t SPIPort::SendByte(uint8_t byte)
 	SERCOM0->SPI.DATA.reg = byte;
 	while(SERCOM0->SPI.INTFLAG.bit.RXC == 0);
 	return SERCOM0->SPI.DATA.reg;
+}
+
+void SERCOM0_Handler()
+{
+	
 }
