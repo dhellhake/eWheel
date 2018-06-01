563dea971695188cbd36fd0d999632cd36e4bbd1
dhellhake <dominik_hellhake@msn.com>
2018-06-01 10:38:10 +0200

    Implemented: Two row printing of numbers

--- EmbeddedSoftware/SandBox/SystemMonitor/SSD1306/SSD1306.h
+++ EmbeddedSoftware/SandBox/SystemMonitor/SSD1306/SSD1306.h
@@ -9,6 +9,7 @@
 #ifndef __SSD1306_H__
 #define __SSD1306_H__
 
+#include <math.h>
 #include "..\..\LowLevel\I2C\I2C.h"
 
 #define SLAVE_ADDR 0b0111100
@@ -20,18 +21,22 @@ public:
 protected:
 private:
 static uint8_t FrameBuffer[513];
+static uint8_t ASCII[320];
 
 //functions
 public:
 	static void Init();
 	static void Update();
-	static void WriteChar16(uint8_t *chr);
+	static void WriteInt(uint16_t number, uint8_t row);
 protected:
 	SSD1306();
 	
 	
 	~SSD1306();
 private:
+	static void ClearPage(uint8_t page);
+	static void WriteChar(uint8_t *chr, uint16_t index);
+
 	SSD1306( const SSD1306 &c );
 	SSD1306& operator=( const SSD1306 &c );
 
