430b37d7ff39051e2128789aaf1145049f586ff1
dhellhake <dominik_hellhake@msn.com>
2018-06-26 23:20:45 +0200

    Reduced SPI-Clk-speed
    due to issues running the LSM9DS1 at full 10mhz

--- EmbeddedSoftware/SandBox/LowLevel/SPI/SPI.cpp
+++ EmbeddedSoftware/SandBox/LowLevel/SPI/SPI.cpp
@@ -19,16 +19,6 @@ SPIPort::~SPIPort()
 
 void SPIPort::InitSERCOM()
 {
-	// max 10Mhz
-	// MSB first
-	// CPOL=0 (~SERCOM_SPI_CTRLA_CPOL)
-	// CPHA=0 (~SERCOM_SPI_CTRLA_CPHA)
-	// Module SERCOM0
-	// PINMUX_PA08C_SERCOM0_PAD0
-	// PINMUX_PA09C_SERCOM0_PAD1
-	// PINMUX_PA10C_SERCOM0_PAD2
-	// PINMUX_PA11C_SERCOM0_PAD3
-
 	//Enable Clock for SERCOM0
 	//Set bits in the clock mask for an APBx bus.
 	MCLK->APBCMASK.bit.SERCOM0_ = 1;
@@ -54,7 +44,7 @@ void SPIPort::InitSERCOM()
 								
 	SERCOM0->SPI.CTRLB.reg =	(1 << SERCOM_SPI_CTRLB_RXEN_Pos);
 		
-	SERCOM0->SPI.BAUD.reg = (System::GetGCLK_Hz(SERCOM0_GCLK_ID_CORE) / (2 * 10000000)) - 1;
+	SERCOM0->SPI.BAUD.reg = (System::GetGCLK_Hz(SERCOM0_GCLK_ID_CORE) / (2 * 9000000)) - 1;
 	
 	SERCOM0->SPI.CTRLA.bit.ENABLE = 1;
 	while(SERCOM0->SPI.SYNCBUSY.bit.ENABLE);
