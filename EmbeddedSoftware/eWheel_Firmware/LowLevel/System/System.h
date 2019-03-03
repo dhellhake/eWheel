e5e7cbbf0d413ce198f0997aec41062803cc3ceb
dhellhake <dominik_hellhake@msn.com>
2019-03-03 16:54:54 +0100

    Added long-division to System

--- EmbeddedSoftware/eWheel_Firmware/LowLevel/System/System.h
+++ EmbeddedSoftware/eWheel_Firmware/LowLevel/System/System.h
@@ -56,6 +56,33 @@ public:
 
 		return (float)DIVAS->RESULT.reg;
 	}
+	
+	/*
+	* internal Calculate 64 bit division, ref can be found in
+	* http://en.wikipedia.org/wiki/Division_algorithm#Long_division
+	*/
+	static inline uint64_t long_division(uint64_t n, uint64_t d)
+	{
+		int32_t i;
+		uint64_t q = 0, r = 0, bit_shift;
+		
+		for (i = 63; i >= 0; i--)
+		{
+			bit_shift = (uint64_t)1 << i;
+			r = r << 1;
+			if (n & bit_shift)
+			{
+				r |= 0x01;
+			}
+			
+			if (r >= d)
+			{
+				r = r - d;
+				q |= bit_shift;
+			}
+		}
+		return q;
+	}
 		
 private:
 	System();
